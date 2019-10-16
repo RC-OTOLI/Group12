31b2ecd857f8b453c14a05fbcbccd35e50cdc25d
Raymond Chin <chin.raymond.andrew@gmail.com>
2019-10-16 15:13:23 -0700

    Removed unnecessary files

--- py2.py
+++
@@ -1,8 +0,0 @@
-#Tupel "()" cant change and monify   
-# # Tupel start with zero 
-coordinates = (4,5)
-# index[0] = 4, and index[1] = 5
-print(coordinates[0])
-# remember Tupel and List are different
-
-coordinatesList = [(4,5),(6,7),(8,9)]
