67d5ab3c4ce0111ffd270da4fb525767ef38d39c
Raymond Chin <chin.raymond.andrew@gmail.com>
2019-10-21 16:16:41 -0700

    Moved files in myflashapp to top level; removed myflashapp folder

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 # app.debug = True
 @app.route('/')
+@app.route('/login')
 def index():
     return render_template('home.html')
+
+
+@app.route('/TransactionHistory')
+def transaction_history():
+    return render_template('TransactionHistory.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     # testing run server with command >> python app.py
