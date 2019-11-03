d0f9ff62efb2d23b4172ae62dcbd3c55eea25ea6
Peeradech <p.abhidechsakhulchai@gmail.com>
2019-11-03 08:47:03 -0800

    remake, added functions for db

--- app.py
+++ app.py
@@ -1,18 +1,84 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I will hiding my secret key for now'
+# for the secret key you have to install SQLAlchemy for the access and need to create the key for the data base from
+#  sQlite, then using the terminal >>python >> from <your local py> import db >> db.create_all()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'signin'
 
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    image_file = db.Column(db.String(15), nullable=False,default='#jpg')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+    
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 # app.debug = True
 @app.route('/')
-@app.route('/login')
+@app.route('/home')
 def index():
     return render_template('home.html')
 
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    form =LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('TransactionHistory'))
+
+        return '<h1>Invalid username or password</h1>'
+    return render_template('signin.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>Welcome the new user!</h1>'
+    return render_template('signup.html', form=form)
 
 @app.route('/TransactionHistory')
 def transaction_history():
     return render_template('TransactionHistory.html')
+
+
+
 
 
 if __name__ == '__main__':
