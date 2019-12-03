587e4429ce76468990bac2aa58c47eba6b18c7e3
Peeradech <p.abhidechsakhulchai@gmail.com>
2019-12-03 14:16:52 -0800

    test with LF

--- app/templates/forms.py
+++ app/templates/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, SubmitField
+from wtforms import StringField, IntegerField, BooleanField, SubmitField,PasswordField
 from wtforms.validators import ValidationError, InputRequired, Email, Length, EqualTo
 from app.models import User
 
@@ -7,8 +7,6 @@ class AddForm(FlaskForm):
     amount = IntegerField('username', validators=[InputRequired(), Length(min=0, max=15)])
     description = StringField('password', validators=[InputRequired(), Length(min=0, max=80)])
    
-
-    
 class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
